3a37b96e8e49484e9c948f52da6c168e40478d1a
ahewbu <shlnsn@outlook.com>
2023-10-25 00:44:56 +0400

    Fixed rating parsing

--- lab1_pp.py
+++ lab1_pp.py
@@ -7,22 +7,24 @@ header = {
     'referer':'https://www.ya.ru/'
 }
 url = "https://www.livelib.ru/reviews/~2#reviews"
-a = requests.get(url, headers={"User-Agent":"Mozilla/5.0"}, allow_redirects=True)
-output = open('t.txt', 'w')
-output.write(str(a.content))
-output.close()
+a = requests.get(url, headers={"User-Agent":"Mozilla/5.0"}, allow_redirects=False)
+a = a.content.decode('utf-8')
+
+with open('t.txt', 'w') as output:
+    output.write(str(a))
+
 
 with open("t.txt", "r") as f:
     e = f.read()
 
 
-lines = re.findall(r'<div class=\"lenta-card__rating\">.{45}<span>.{3}</span>.{24}</div>', e)
+lines = re.findall(r'<div class=\"lenta-card__rating\">\D{42}<span>\S{3}</span>\D{23}</div>', e)
 result = {}
 for line in lines:
     temp = re.sub('<div class="lenta-card__rating">','', line)
-    temp = re.sub('.{45}<span>','', temp)
+    temp = re.sub('\D{42}<span>','', temp)
     temp = re.sub('</span>', '', temp)
-    temp = re.sub('.{24}</div>', '', temp)
+    temp = re.sub('\D{23}</div>', '', temp)
 
     print(temp)
 
